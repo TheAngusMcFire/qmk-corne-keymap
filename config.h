383435c9efb41313fb9daa06373a529c056d6b3a
Christian Rieger <christian.rieger@student.tugraz.at>
2024-05-09 15:28:03 +0200

    [main] change the tapping timeout

--- config.h
+++ config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
 // #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader//#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 200
+#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
